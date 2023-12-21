ca40f45bfc73031c4461d31574104ad658b803cb
MZainGamildien <102467824+MZainGamildien@users.noreply.github.com>
2023-12-21 21:21:43 +0200

    registration page

--- process_form.php
+++ process_form.php
@@ -2,34 +2,52 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$studentEmail = $_POST['studentEmail'];
-$phoneNumber = $_POST['phoneNumber'];
-$password = $_POST['password'];
+// Function to sanitize input data
+function sanitizeData($data)
+{
+    return htmlspecialchars(stripslashes(trim($data)));
+}
 
-// Database connection
-$conn = new mysqli('localhost', 'root', 'password', 'ems_db');
-if ($conn->connect_error) {
-    echo "$conn->connect_error";
-    die("Connection Failed : " . $conn->connect_error);
-} else {
-    // Corrected SQL query and prepare statement
-    $stmt = $conn->prepare("INSERT INTO registration (firstName, lastName, studentEmail, phoneNumber, password) VALUES (?, ?, ?, ?, ?)");
-    // Binding parameters to the prepared statement
-    $stmt->bind_param("sssss", $firstName, $lastName, $studentEmail, $phoneNumber, $password);
+// Checking if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = sanitizeData($_POST['firstName']);
+    $lastName = sanitizeData($_POST['lastName']);
+    $studentEmail = sanitizeData($_POST['studentEmail']);
+    $phoneNumber = sanitizeData($_POST['phoneNumber']);
+    $password = sanitizeData($_POST['password']);
 
-    // Executing the prepared statement
-    $execval = $stmt->execute();
-
-    if ($execval) {
-        header("Location: signup.html");
-        exit();
+    // Validating for empty fields
+    if (empty($firstName) || empty($lastName) || empty($studentEmail) || empty($phoneNumber) || empty($password)) {
+        echo "Please fill in all fields.";
+    } elseif (!filter_var($studentEmail, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format.";
     } else {
-        echo "Error: " . $stmt->error;
-    }
+        // Database connection
+        $conn = new mysqli('localhost', 'root', '', 'ems_db');
+        if ($conn->connect_error) {
+            echo "$conn->connect_error";
+            die("Connection Failed : " . $conn->connect_error);
+        } else {
+            // Corrected SQL query and prepare statement
+            $stmt = $conn->prepare("INSERT INTO registration (firstName, lastName, studentEmail, phoneNumber, password) VALUES (?, ?, ?, ?, ?)");
+            // Binding parameters to the prepared statement
+            $stmt->bind_param("sssss", $firstName, $lastName, $studentEmail, $phoneNumber, $password);
 
-    $stmt->close();
-    $conn->close();
+            // Executing the prepared statement
+            $execval = $stmt->execute();
+
+            if ($execval) {
+                //alert ("Data uploaded successfully!");
+                // You can redirect the user to another page using header if needed
+                 header("Location: signup.html");
+                 exit();
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+
+            $stmt->close();
+            $conn->close();
+        }
+    }
 }
 ?>
