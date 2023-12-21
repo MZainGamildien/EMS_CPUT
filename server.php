80ca37a2f02eee8b1c32593326222df63a67ed48
athinikau <athinimbiko5@gmail.com>
2023-12-21 19:43:07 +0000

    updated page

--- server.php
+++ server.php
@@ -1,24 +1,19 @@
-
-
-
 <?php
-$firstName=$_POST['firstName'];
-$lastName=$_POST['lastName'];
-$studentEmail=$_POST['studentEmail'];
-$phoneNumber=$_POST['phoneNumber'];
+
+$Email=$_POST['Email'];
 $password=$_POST['password'];
 
 // Database connection
-$conn = new mysqli('localhost','root','password','ems_db');
+$conn = new mysqli('localhost','root','','ems_db');
 if($conn->connect_error){
     echo "$conn->connect_error";
     die("Connection Failed : ". $conn->connect_error);
 } else {
     $stmt = $conn->prepare("insert into registration(firstName, lastName, studentEmail, phoneNumber, password) values(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssi", $firstName, $lastName, $studentEmail, $phoneNumber, $password);
+    $stmt->bind_param("sssssi",  $Email,$password);
     $execval = $stmt->execute();
     echo $execval;
-    echo "Registration successfully...";
+    echo "Login successfully...";
     $stmt->close();
     $conn->close();
 }
