e178196d56885131b0f3ff19a3a8f7e95be52dc8
athinikau <athinimbiko5@gmail.com>
2023-12-22 00:22:39 +0000

    updated login and server

---
+++ login.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="process_form.php">
+  	<div class="input-group">
+  		<label>Email</label>
+  		<input type="text" id="Email" placeholder="Enter  Email" name="email" required>
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="text" id="password" placeholder="Enter password" name="password" required>
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a sign up? <a href="registration.html">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
--- server.php
+++ server.php
@@ -1,21 +1,33 @@
 <?php
 
-$Email=$_POST['Email'];
+$email=$_POST['Email'];
 $password=$_POST['password'];
 
-// Database connection
-$conn = new mysqli('localhost','root','','ems_db');
-if($conn->connect_error){
-    echo "$conn->connect_error";
-    die("Connection Failed : ". $conn->connect_error);
-} else {
-    $stmt = $conn->prepare("insert into registration(firstName, lastName, studentEmail, phoneNumber, password) values(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssi",  $Email,$password);
-    $execval = $stmt->execute();
-    echo $execval;
-    echo "Login successfully...";
-    $stmt->close();
-    $conn->close();
+
+if (empty($email) || empty($password)) {
+    echo "Please fill in all fields.";
+} elseif (!filter_var($studentEmail, FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid email format.";
 }
+else{
+    $conn = new mysqli('localhost', 'root', '', 'ems_db');
+        if ($conn->connect_error) {
+            echo "$conn->connect_error";
+            die("Connection Failed : " . $conn->connect_error);
+        } else {
+            // Database connection
+            $stmt = $conn->prepare("insert into login(Email, password) values(?, ?)");
+            $stmt->bind_param("sssssi",  $Email,$password);
+            $execval = $stmt->execute();
+            echo $execval;
+            echo "Login successfully...";
+            $stmt->close();
+            $conn->close();
+    
+        }
+
+
+    }
+
 
 ?>
